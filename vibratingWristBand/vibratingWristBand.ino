0bf294d76176c01f5b9e4d2ca26ee1759b399601
Michael Cumming <michael@ambitiouscity.com>
2014-04-22 11:49:12 -0400

    added for loops to code

--- vibratingWristBand/vibratingWristBand.ino
+++ vibratingWristBand/vibratingWristBand.ino
@@ -4,37 +4,52 @@
  */
  
 // Pin numbers and their names
-int vib1 = 13;
-int vib2 = 12;
-int vib3 = 11;
-int vib4 = 10;
-int vib5 = 9;
-int vib6 = 8;
-int vib7 = 7;
-int vib8 = 6;
+//int vib1 = 1;
+//int vib2 = 2;
+//int vib3 = 3;
+//int vib4 = 4;
+//int vib5 = 5;
+//int vib6 = 6;
+//int vib7 = 7;
+//int vib8 = 8;
+//int vib9 = 9;
+//int vib10 = 10;
+//int vib11 = 11;
+//int vib12 = 12;
+//int vib13 = 13;
+//int vib14 = 14;
+//int vib15 = 15;
 
-int numVIBs = 8;
+
+int numPins = 15;
 int delayMid = 200;
 int delayAtEnd = 500;
 
 // the setup routine runs once when you press reset:
 void setup() {                
   // initialize the digital pin as an output.
-  pinMode(vib1, OUTPUT);   
-  pinMode(vib2, OUTPUT);
-  pinMode(vib3, OUTPUT);   
-  pinMode(vib4, OUTPUT);  
-  pinMode(vib5, OUTPUT);   
-  pinMode(vib6, OUTPUT);
-  pinMode(vib7, OUTPUT);   
-  pinMode(vib8, OUTPUT); 
+  int i;
+  for(i = 0; 1 < 14; i++) {
+    pinMode(i, OUTPUT); 
+  }
+//  pinMode(vib1, OUTPUT);   
+//  pinMode(vib2, OUTPUT);
+//  pinMode(vib3, OUTPUT);   
+//  pinMode(vib4, OUTPUT);  
+//  pinMode(vib5, OUTPUT);   
+//  pinMode(vib6, OUTPUT);
+//  pinMode(vib7, OUTPUT);   
+//  pinMode(vib8, OUTPUT); 
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  digitalWrite(vib1, HIGH); //turn on
-  delay(delayMid);
-  digitalWrite(vib1, LOW);  //turn off
+  for(i = 0; 1 < 14; i++) {
+    digitalWrite(vib1, HIGH); //turn on
+    delay(delayMid);
+    digitalWrite(vib1, LOW);  //turn off
+  }
+  
   
   digitalWrite(vib2, HIGH);
   delay(delayMid);
